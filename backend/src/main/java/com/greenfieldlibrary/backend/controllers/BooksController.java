3d1f02b6bc0c2d97dfd0196108ad43e102daea43
evavelasco <evapibiyal@gmail.com>
2024-02-13 12:57:37 +0100

    añadida rama métodos

---
+++ backend/src/main/java/com/greenfieldlibrary/backend/controllers/BooksController.java
@@ -0,0 +1,13 @@
+package com.greenfieldlibrary.backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BooksController {
+    @Autowired
+    private BookRepository bookRepository;
+
+}
