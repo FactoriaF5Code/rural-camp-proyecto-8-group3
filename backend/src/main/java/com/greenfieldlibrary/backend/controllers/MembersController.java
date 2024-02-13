5a3160b3a82e716e2fea2e74502a7e4a4f13df9c
srb4by <oliversanchezcasiano@gmail.com>
2024-02-13 14:51:00 +0100

    cambios en MembersController.java en el apartado @RequestMapping

--- backend/src/main/java/com/greenfieldlibrary/backend/controllers/MembersController.java
+++ backend/src/main/java/com/greenfieldlibrary/backend/controllers/MembersController.java
@@ -1,6 +1,10 @@
 package com.greenfieldlibrary.backend.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping(/members)
