82bc14dd28794099769af416e65a25e4bc1064ce
San-QP <s.rebollar.g@gmail.com>
2024-02-15 18:37:14 +0100

    Toogle Buttons (libros/usuarios) en el buscador con clases configuradas

--- FRONTEND/src/components/Searcher/Searcher.jsx
+++ FRONTEND/src/components/Searcher/Searcher.jsx
@@ -5,10 +5,7 @@ export const Searcher = () => {
   return (
     <section className="searchContainer">
       <div className='searchContainer__Field'>
-        <div className='searchContainer__Field-icon'> 
-          {/* <img src="/src/assets/bookIcon.svg" alt="libro icono" /> */}
-          <VerticalToggleButtons />
-        </div>
+      <VerticalToggleButtons />
         <input
           type="search"
           className="searchContainer__Field-Input"
@@ -20,25 +17,3 @@ export const Searcher = () => {
     </section>
   )
 }
-
-{/* <section className="searchContainer">
-<div className='searchContainer__Field'>
-  <div className='searchContainer__Field-icon'> 
-    <img src="/src/assets/bookIcon.svg" alt="libro icono" />
-  </div>
-
-
-  <input
-    type="search"
-    className="searchContainer__Field-Input"
-    placeholder="Búsqueda de libro por título, autor o ISBN"/>
-  <button className="searchContainer__Field-Button">
-    <img src="/src/assets/searchIcon.svg" alt="búsqueda icono" />
-  </button>        
-</div>
-</section> */}
-
-        // <select name="searcher" id="searcher" className="searchContainer__Field-select">
-        //   <option value="books">Libros</option>
-        //   <option value="users">Socios</option>
-        // </select>
--- FRONTEND/src/components/Searcher/VerticalToogleButtons.jsx
+++ FRONTEND/src/components/Searcher/VerticalToogleButtons.jsx
@@ -1,30 +1,62 @@
+import './Searcher.css';
 import PersonIcon from '@mui/icons-material/Person';
 import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import { useState } from "react";
-import './Searcher.css'
+import { useEffect, useState } from 'react';
 
-export const VerticalToggleButtons= () => {
-    const [view, setView] = useState("list");
+export const VerticalToggleButtons = () => {
+  const [buttonActive, setButtonActive] = useState(false);
+  const [userButtonClicked, setUserButtonClicked] = useState(false);
+  const [booksButtonClicked, setBooksButtonClicked] = useState(false);
 
-  const handleChange = (event, nextView) => {
-    setView(nextView);
+
+  useEffect(() => {
+    // Cambia el estado de buttonActive a true cuando se carga la página
+    setButtonActive(true);
+  }, []);
+
+  // Maneja la clase del botón de libros de toogleButton a toggleButton-active
+  const handleUserButtonClick = () => {
+    setUserButtonClicked(true);
+  };
+
+  // Maneja la clase del botón de socios de toogleButton a toggleButton-active
+  const handleBooksButtonClick = () => {
+    setBooksButtonClicked(true);
   };
 
   return (
-    <ToggleButtonGroup
-      orientation="vertical"
-      value={view}
-      exclusive
-      onChange={handleChange}
-    >
-      <ToggleButton value="book" aria-label="book" className="toogleButton">
-        <CollectionsBookmarkIcon className='toogleIcon' />
-      </ToggleButton>
-      <ToggleButton value="user" aria-label="user" className="toogleButton">
-        <PersonIcon className='toogleIcon' />
-      </ToggleButton>
-    </ToggleButtonGroup>
+    <div>
+      <ToggleButtonGroup
+        value="books"
+        exclusive
+      >
+        <ToggleButton
+          value="book"
+          aria-label="book"
+            //Se verifican dos condiciones: que buttonActive sea true (lo que ocurre al cargar la página)
+            //Y que el botón de socios no haya sido clicado
+            //Si ambas condiciones son correctas se aplica la clase toogleButton-active
+          className={buttonActive && !userButtonClicked ? 'toogleButton-active' : 'toogleButton'}
+            //Al hacer clic también se cambia la clase a toogleButton-active
+          onClick={handleBooksButtonClick}
+        >
+          <CollectionsBookmarkIcon className='toogleIcon' />
+        </ToggleButton>
+        <ToggleButton
+          value="users"
+          aria-label="users"
+            //Se verifican dos condiciones: que el botón de usuarios se haya clicado
+            // que el botón de libros no haya sido clicado
+            //si ambas son correctas se cambia la clase a toggleButton-active
+          className={userButtonClicked && !booksButtonClicked? 'toogleButton-active' : 'toogleButton'}
+           //Al hacer clic se cambia la clase a toogleButton-active
+          onClick={handleUserButtonClick}>
+          <PersonIcon className='toogleIcon' />
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </div>
   );
-}
+};
+
