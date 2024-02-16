d63c4edebe3fc247c55b69b2638fda410ed22443
evavelasco <evapibiyal@gmail.com>
2024-02-16 11:33:49 +0100

    silenciar lendings

---
+++ backend/src/main/java/com/greenfieldlibrary/backend/controllers/LendingsRequest.java
@@ -0,0 +1,43 @@
+// package com.greenfieldlibrary.backend.controllers;
+
+// import java.util.Date;
+
+// public class LendingsRequest {
+
+// private Long idBooks;
+// private Long idMember;
+// private Date dataLending;
+// private Date dataReturn;
+// public LendingsRequest(Long idBooks, Long idMember, Date dataLending, Date
+// dataReturn) {
+// this.idBooks = idBooks;
+// this.idMember = idMember;
+// this.dataLending = dataLending;
+// this.dataReturn = dataReturn;
+// }
+// public Long getIdBooks() {
+// return idBooks;
+// }
+// public void setIdBooks(Long idBooks) {
+// this.idBooks = idBooks;
+// }
+// public Long getIdMember() {
+// return idMember;
+// }
+// public void setIdMember(Long idMember) {
+// this.idMember = idMember;
+// }
+// public Date getDataLending() {
+// return dataLending;
+// }
+// public void setDataLending(Date dataLending) {
+// this.dataLending = dataLending;
+// }
+// public Date getDataReturn() {
+// return dataReturn;
+// }
+// public void setDataReturn(Date dataReturn) {
+// this.dataReturn = dataReturn;
+// }
+
+// }
