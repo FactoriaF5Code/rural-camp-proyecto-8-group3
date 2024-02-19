041ff12dda2ff7d20e15ffdfbcaf41e855aa9383
enolsurfer <39172327+enolsurfer@users.noreply.github.com>
2024-02-19 13:06:18 +0100

    tabla con estilos

--- FRONTEND/src/components/Table/Table.jsx
+++ FRONTEND/src/components/Table/Table.jsx
@@ -21,26 +21,20 @@ function FixedHeaderTable({ activeButton }) {
         let response;
         if (activeButton === "books") {
           response = await axios.get("http://localhost:9000/books");
-          setTableHeaders([
-            "ID",
-            "ISBN",
-            "Título",
-            "Autor",
-            "Estado"
-          ]);
+          setTableHeaders(["ID", "ISBN", "Título", "Autor", "Estado"]);
         } else if (activeButton === "members") {
           response = await axios.get("http://localhost:9000/members");
-          setTableHeaders([
-            "ID",
-            "Nombre",
-            "Apellidos",
-            "Email",
-            "Teléfono"
-          ]);
+          setTableHeaders(["ID", "Nombre", "Apellidos", "Email", "Teléfono"]);
         }
 
         if (response && response.data) {
-          setData(response.data);
+          const modifiedData = response.data.map((item) => {
+            if (item.status === "En préstamo") {
+              return { ...item, status: "Devolución" };
+            }
+            return item;
+          });
+          setData(modifiedData);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -56,8 +50,10 @@ function FixedHeaderTable({ activeButton }) {
         <Table aria-label="fixed header table">
           <TableHead>
             <TableRow>
-              {tableHeaders.map(header => (
-                <TableCell key={header} className="table-cell-header">{header}</TableCell>
+              {tableHeaders.map((header) => (
+                <TableCell key={header} className="table-cell-header">
+                  {header}
+                </TableCell>
               ))}
             </TableRow>
           </TableHead>
@@ -70,11 +66,24 @@ function FixedHeaderTable({ activeButton }) {
                     <TableCell className="table-cell">{row.isbn}</TableCell>
                     <TableCell className="table-cell">{row.title}</TableCell>
                     <TableCell className="table-cell">{row.author}</TableCell>
-                    <TableCell className="table-cell">{row.status}</TableCell>
+                    <TableCell className={`table-cell ${row.status === "Disponible" ? "available" : "not-available"}`}>
+                      <div className="status-container">
+                        <div className={`status-circle ${row.status === "Disponible" ? "available" : "not-available"}`}></div>
+                        {row.status === "Disponible" || row.status === "Devolución" ? (
+                          <button onClick={() => window.location.href = row.status === "Disponible" ? "http://localhost:9000/prestamo/{id}" : "http://localhost:9000/devolucion-ok/{id}"}>
+                            {row.status}
+                          </button>
+                        ) : (
+                          <span>{row.status}</span>
+                        )}
+                      </div>
+                    </TableCell>
                   </React.Fragment>
                 ) : (
                   <React.Fragment>
-                    <TableCell className="table-cell">{row.idMembers}</TableCell>
+                    <TableCell className="table-cell">
+                      {row.idMembers}
+                    </TableCell>
                     <TableCell className="table-cell">{row.name}</TableCell>
                     <TableCell className="table-cell">{row.lastName}</TableCell>
                     <TableCell className="table-cell">{row.email}</TableCell>
